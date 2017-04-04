28032dad09e70c987c6c2a7ff7099a010ad36c60
surabhi109 <surabhi.srivatsava000@gmail.com>
2017-04-04 19:59:25 +0530

    Create myapp.module.js

---
+++ Music_updated/app/myapp.module.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+    angular
+        .module('myApp', ['ui.router','spotify'
+		])
+		  .config(homeConfig);
+    
+
+  homeConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function homeConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider
+        .when('', '/')
+        .otherwise('/');
+
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'app/features/home/home.tpl.html',
+      controller: 'MusicController',
+      controllerAs: 'vm'
+    });
+  }
+})();
